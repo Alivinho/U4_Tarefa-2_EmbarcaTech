f3e73399bfd6664e760d5a20a0632b4f4f82ecc3
Alisson Livio <livilia74629@gmail.com>
2025-02-03 11:09:54 -0300

    feat: função para inciar os leds

--- Atividade_1_TemporizadorPeriodico/main.c
+++ Atividade_1_TemporizadorPeriodico/main.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+#define LED_RED_PIN 13
+#define LED_YELLOW_PIN 12
+#define LED_GREEN_PIN 11
 
+void initLeds();
 
 int main()
 {
@@ -11,4 +15,15 @@ int main()
         printf("Hello, world!\n");
         sleep_ms(1000);
     }
+}
+
+//Função para Inicializar os LEDS
+void initLeds(){
+    gpio_init(LED_RED_PIN);
+    gpio_init(LED_YELLOW_PIN);
+    gpio_init(LED_GREEN_PIN);
+    
+    gpio_set_dir(LED_YELLOW_PIN, GPIO_OUT);
+    gpio_set_dir(LED_YELLOW_PIN, GPIO_OUT);
+    gpio_set_dir(LED_GREEN_PIN, GPIO_OUT);
 }
