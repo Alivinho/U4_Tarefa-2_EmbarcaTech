c576e36a5fb5f3209028588016e82181bf40bc64
Alisson Livio <livilia74629@gmail.com>
2025-02-04 09:26:51 -0300

    feat: mudança na estrutura. Adição de funções para controle do código

--- Atividade_2_TemporizadorDisparo/main.c
+++ Atividade_2_TemporizadorDisparo/main.c
@@ -1,29 +1,42 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
-#include "pico/time.h"
+#include "hardware/gpio.h"
+#include "hardware/timer.h"
 
 #define LED_RED_PIN 13
 #define LED_BLUE_PIN 12
 #define LED_GREEN_PIN 11
-
 #define BUTTON_A_PIN 5
 
+// Variáveis de controlr
+volatile bool sistema_ativo = false;
+volatile uint32_t ultimo_tempo = 0;
 
 
-int main(){
+// Protótipos das funções utilizadas
+void setup();
+void botao_irq_handler(uint gpio, uint32_t events);
+int64_t desligar_leds_callback(alarm_id_t id, void *user_data);
 
+int main() {
     stdio_init_all();
 
-    // Inicialização dos componentes (Leds e Botão)
+    // Função para inicialização dos componentes - LED e Botão 
     setup();
 
-    while (true) {
-        printf("Hello, world!\n");
+    // Função para Rotina de Interrupção do Botão 
+    gpio_set_irq_enabled_with_callback(BUTTON_A_PIN, GPIO_IRQ_EDGE_FALL, true, &botao_irq_handler);
+    
+    while (1) {
+         if (sistema_ativo) {
+            printf("Sistema ativo: LEDs em processo de desligamento.");
+        } else {
+            printf("Sistema inativo: Aguardando acionamento do botão.");
+        }
         sleep_ms(1000);
     }
 }
 
-// Função para inicializar o LED RGB e o Botão
 void setup() {
     gpio_init(BUTTON_A_PIN);
     gpio_set_dir(BUTTON_A_PIN, GPIO_IN);
@@ -37,4 +50,39 @@ void setup() {
     
     gpio_init(LED_GREEN_PIN);
     gpio_set_dir(LED_GREEN_PIN, GPIO_OUT);
+}
+
+void botao_irq_handler(uint gpio, uint32_t events) {
+    uint32_t tempo_atual = to_ms_since_boot(get_absolute_time());
+    if (tempo_atual - ultimo_tempo < 200) return;
+    ultimo_tempo = tempo_atual;
+    
+    if (!sistema_ativo) {
+        sistema_ativo = true;
+        gpio_put(LED_GREEN_PIN, 1);
+        gpio_put(LED_BLUE_PIN, 1);
+        gpio_put(LED_RED_PIN, 1);
+       
+        add_alarm_in_ms(3000, desligar_leds_callback, NULL, false);
+    }
+}
+
+int64_t desligar_leds_callback(alarm_id_t id, void *user_data) {
+    static int estado = 0;
+    
+    if (estado == 0) {
+        gpio_put(LED_GREEN_PIN, 0);
+        
+        estado++;
+        add_alarm_in_ms(3000, desligar_leds_callback, NULL, false);
+    } else if (estado == 1) {
+        gpio_put(LED_BLUE_PIN, 0);
+        estado++;
+        add_alarm_in_ms(3000, desligar_leds_callback, NULL, false);
+    } else {
+        gpio_put(LED_RED_PIN, 0);
+        sistema_ativo = false;
+        estado = 0;
+    }
+    return 0;
 }
